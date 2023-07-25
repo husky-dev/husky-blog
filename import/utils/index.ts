ab26bba34aaceb6d004fdee04b1272633372c3c6
Jaroslav Khorishchenko <websnipter@gmail.com>
2023-07-25 11:33:09 +0300

    Import moved to the separate project since it becomes too big

--- import/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./assets";
-export * from "./str";
-export * from "./fs";
-export * from "./log";
-export * from "./md";
--- import/utils/log.ts
+++
@@ -1,5 +0,0 @@
-export const log = {
-  debug: (...args: unknown[]) => console.log("[-]:", ...args),
-  info: (...args: unknown[]) => console.log("[+]:", ...args),
-  err: (...args: unknown[]) => console.log("[x]:", ...args),
-};
